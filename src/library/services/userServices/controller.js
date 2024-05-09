c77e052cbb83bab9eac045f605096c552372ffe4
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-05-09 17:10:30 +0300

    create getUserById and getUserByEmail

--- src/library/services/userServices/controller.js
+++ src/library/services/userServices/controller.js
@@ -33,3 +33,29 @@ export const getAllUsers =
       }
     }
   };
+
+  export const getUserById = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await Users.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
+
+  export const getUserByEmail = async (req, res) => {
+    const { email } = req.params;
+    try {
+      const user = await Users.findOne({email: email});
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      res.json(user);
+    }catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
--- src/library/services/userServices/router.js
+++ src/library/services/userServices/router.js
@@ -5,5 +5,7 @@ const UsersRouter = express.Router();
 
 UsersRouter.get("/users", Controller.getAllUsers);
 UsersRouter.post("/users", Controller.createUser);
+UsersRouter.get("/userById/:id", Controller.getUserById);
+UsersRouter.get("/userByEmail/:email", Controller.getUserByEmail);
 
 export default UsersRouter;
