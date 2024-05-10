0baef0fa72bc52d5bf986d6e245344f446bcd089
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-05-10 17:36:55 +0300

    creating a delete and update users by user id. controller.js router.js

--- src/library/services/userServices/controller.js
+++ src/library/services/userServices/controller.js
@@ -1,8 +1,6 @@
 import Users from "../../modules/usersModule.js";
 
-export const getAllUsers =
-  ("/users",
-  async (req, res) => {
+export const getAllUsers =  async (req, res) => {
     try {
       console.log("inside");
       const users = await Users.find();
@@ -15,47 +13,74 @@ export const getAllUsers =
       console.log("error fetching users:", error);
       res.status(500).send(error.message);
     }
-  });
-
-  export const createUser = async (req, res) => {
-    const userData = req.body;
-    try {
-      const newUser = new Users(userData);
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
-    } catch (error) {
-      if (error.code === 11000 && error.keyValue.email !== undefined) {
-        // If the error is due to a duplicate email
-        res.status(400).json({ error: "Email already exists" });
-      } else {
-        // For other errors
-        res.status(400).json({ error: `Error creating user: ${error.message}` });
-      }
-    }
   };
 
-  export const getUserById = async (req, res) => {
-    const { id } = req.params;
-    try {
-      const user = await Users.findById(id);
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-      res.json(user);
-    } catch (error) {
-      res.status(500).send(error.message);
+export const createUser = async (req, res) => {
+  const userData = req.body;
+  try {
+    const newUser = new Users(userData);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    if (error.code === 11000 && error.keyValue.email !== undefined) {
+      // If the error is due to a duplicate email
+      res.status(400).json({ error: "Email already exists" });
+    } else {
+      // For other errors
+      res.status(400).json({ error: `Error creating user: ${error.message}` });
     }
-  };
+  }
+};
 
-  export const getUserByEmail = async (req, res) => {
-    const { email } = req.params;
-    try {
-      const user = await Users.findOne({email: email});
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-      res.json(user);
-    }catch (error) {
-      res.status(500).send(error.message);
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-  };
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const getUserByEmail = async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await Users.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const deleteUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ message: "User deleted" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const userData = req.body;
+  try {
+    const user = await Users.findByIdAndUpdate(id, userData, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/library/services/userServices/router.js
+++ src/library/services/userServices/router.js
@@ -4,8 +4,14 @@ import express from "express";
 const UsersRouter = express.Router();
 
 UsersRouter.get("/users", Controller.getAllUsers);
-UsersRouter.post("/users", Controller.createUser);
 UsersRouter.get("/userById/:id", Controller.getUserById);
 UsersRouter.get("/userByEmail/:email", Controller.getUserByEmail);
+
+
+UsersRouter.post("/users", Controller.createUser);
+
+UsersRouter.put("/users/:id", Controller.updateUserById);
+
+UsersRouter.delete("/users/:id", Controller.deleteUserById);
 
 export default UsersRouter;
