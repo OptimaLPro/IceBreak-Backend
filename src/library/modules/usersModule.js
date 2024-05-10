9451d959b7cd10933006d3eb1e4ef6890106d33f
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-05-10 23:56:57 +0300

    Adding first name and last name to user schema

--- src/library/modules/usersModule.js
+++ src/library/modules/usersModule.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 const usersSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
