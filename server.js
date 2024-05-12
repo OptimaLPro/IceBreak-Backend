fcdfb880f8c5afe6432478d01085971ee72c00ab
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-05-12 13:39:22 +0300

    changing Categories endpoint

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/games", GamesRouter);
-app.use("/", CategoriesRouter);
+app.use("/categories", CategoriesRouter);
 app.use("/", UsersRouter);
 // Start server
 
