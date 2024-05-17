c181d9f6d74a1d89a8d3889453924c0ccb7cafca
Sahar <96saharyosef@gmail.com>
2024-05-17 11:42:36 +0300

    fixes

--- server.js
+++ server.js
@@ -1,139 +1,133 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import Game from "./src/library/modules/gamesModule.js";
-import dotenv from "dotenv";
-import GamesRouter from './src/library/services/gameServices/Router.js';
-import CategoriesRouter from './src/library/services/categoriesServices/Router.js';
-import http from "http";
-import bodyParser from "body-parser";
-import { Server } from "socket.io";
-import GamesLogRouter from "./src/library/services/gamesLogServices/Router.js";
-import TriviaRouter from "./src/library/services/triviaServices/Router.js";
-dotenv.config();
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import http from 'http'
+import mongoose from 'mongoose'
+import { Server } from 'socket.io'
+import CategoriesRouter from './src/library/services/categoriesServices/Router.js'
+import GamesRouter from './src/library/services/gameServices/Router.js'
+import TriviaRouter from './src/library/services/triviaServices/Router.js'
+import UsersRouter from './src/library/services/userServices/router.js'
 
-const port = 8080;
-const server_port = 3001;
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-const server = http.createServer(app);
+dotenv.config()
+
+const port = 8080
+const server_port = 3001
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const server = http.createServer(app)
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    },
-});
+  cors: {
+    origin: '*',
+  },
+})
 
 server.listen(server_port, () => {
-    console.log(`Server is running on port ${server_port}`);
-});
+  console.log(`Server is running on port ${server_port}`)
+})
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
+app.use('/games', GamesRouter)
+app.use('/categories', CategoriesRouter)
+app.use('/users', UsersRouter)
+app.use('/trivia', TriviaRouter)
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use("/games", GamesRouter);
-app.use("/categories", CategoriesRouter);
-app.use("/users", UsersRouter);
-app.use('/trivia', TriviaRouter);
-
-
-const rooms = {};
+const rooms = {}
 
 io.on('connection', (socket) => {
-    console.log('A user connected ' + socket.id);
+  console.log('A user connected ' + socket.id)
 
-    socket.on('createRoom', (roomData) => {
-        const { gamePIN } = roomData;
-        socket.join(gamePIN);
-        socket.emit('roomCreated', gamePIN);
-        rooms[gamePIN] = {
-            owner: socket.id,
-            players: [],
-        };
-        console.log('Room created with ID:', gamePIN);
-        console.log('Rooms:', rooms);
-    });
+  socket.on('createRoom', (roomData) => {
+    const { gamePIN } = roomData
+    socket.join(gamePIN)
+    socket.emit('roomCreated', gamePIN)
+    rooms[gamePIN] = {
+      owner: socket.id,
+      players: [],
+    }
+    console.log('Room created with ID:', gamePIN)
+    console.log('Rooms:', rooms)
+  })
 
-    socket.on('checkRoom', (roomData) => {
-        const { gamePIN } = roomData;
-        if (rooms[gamePIN]) {
-            socket.emit('roomChecked', true);
-        } else {
-            socket.emit('roomChecked', false);
-        }
-    });
+  socket.on('checkRoom', (roomData) => {
+    const { gamePIN } = roomData
+    if (rooms[gamePIN]) {
+      socket.emit('roomChecked', true)
+    } else {
+      socket.emit('roomChecked', false)
+    }
+  })
 
-    socket.on('getPlayers', ({ gamePIN }) => {
-        if (rooms[gamePIN]) {
-            socket.emit('playersData', { players: rooms[gamePIN].players });
-        }
-    });
+  socket.on('getPlayers', ({ gamePIN }) => {
+    if (rooms[gamePIN]) {
+      socket.emit('playersData', { players: rooms[gamePIN].players })
+    }
+  })
 
-    socket.on('joinRoom', (roomData) => {
-        const { gamePIN, name, avatar } = roomData;
-        rooms[gamePIN].players.push({ id: socket.id, name, avatar });
+  socket.on('joinRoom', (roomData) => {
+    const { gamePIN, name, avatar } = roomData
+    rooms[gamePIN].players.push({ id: socket.id, name, avatar })
 
-        io.to(gamePIN).emit('playerJoined', { id: socket.id, name, avatar });
+    io.to(gamePIN).emit('playerJoined', { id: socket.id, name, avatar })
 
-        const connectedPeopleCount = io.sockets.adapter.rooms.get(gamePIN)?.size || 0;
-        io.to(gamePIN).emit('connectedPeopleCount', connectedPeopleCount);
+    const connectedPeopleCount =
+      io.sockets.adapter.rooms.get(gamePIN)?.size || 0
+    io.to(gamePIN).emit('connectedPeopleCount', connectedPeopleCount)
 
-        socket.join(gamePIN);
-        socket.emit('roomJoined', gamePIN);
-        console.log('Rooms:', rooms);
-    });
+    socket.join(gamePIN)
+    socket.emit('roomJoined', gamePIN)
+    console.log('Rooms:', rooms)
+  })
 
-    socket.on('checkRoomOwner', (data) => {
-        const { gamePIN } = data;
-        if (rooms[gamePIN] && rooms[gamePIN].owner === socket.id) {
-            socket.emit('isRoomOwner', true);
-        } else {
-            socket.emit('isRoomOwner', false);
-        }
-    });
+  socket.on('checkRoomOwner', (data) => {
+    const { gamePIN } = data
+    if (rooms[gamePIN] && rooms[gamePIN].owner === socket.id) {
+      socket.emit('isRoomOwner', true)
+    } else {
+      socket.emit('isRoomOwner', false)
+    }
+  })
 
-    socket.on('startGame', (data) => {
-        console.log('Game started with ID:', data.gamePIN);
-        io.to(data.gamePIN).emit('gameStarted', true);
-    });
+  socket.on('startGame', (data) => {
+    console.log('Game started with ID:', data.gamePIN)
+    io.to(data.gamePIN).emit('gameStarted', true)
+  })
 
-
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('A user disconnected ' + socket.id);
-        // Find and remove the disconnected player from the players array
-        Object.keys(rooms).forEach((gamePIN) => {
-            rooms[gamePIN].players = rooms[gamePIN].players.filter(
-                (player) => player.id !== socket.id
-            );
-            // Emit event to inform all clients in the room that a player left
-            io.to(gamePIN).emit('playerLeft', { id: socket.id });
-        });
-        console.log('Rooms:', rooms);
-    });
-});
-
-
-
-
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected ' + socket.id)
+    // Find and remove the disconnected player from the players array
+    Object.keys(rooms).forEach((gamePIN) => {
+      rooms[gamePIN].players = rooms[gamePIN].players.filter(
+        (player) => player.id !== socket.id
+      )
+      // Emit event to inform all clients in the room that a player left
+      io.to(gamePIN).emit('playerLeft', { id: socket.id })
+    })
+    console.log('Rooms:', rooms)
+  })
+})
 
 // Start server
 
+const mongoUserName = process.env.MONGO_USER_NAME
+const mongoPassword = process.env.MONGO_PASSWORD
+const mongoUrl = process.env.MONGO_CONNECT_URL
+
 mongoose
-    .connect(
-       `mongodb+srv://${mongoUserName}:${mongoPassword}@${mongoUrl}/IceBreak?retryWrites=true&w=majority`,
-    )
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`DB is Connected on port ${port}!`);
-        });
+  .connect(
+    `mongodb+srv://${mongoUserName}:${mongoPassword}@${mongoUrl}/IceBreak?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`DB is Connected on port ${port}!`)
     })
-    .catch((error) => {
-        console.log(error);
-    });
-  
+  })
+  .catch((error) => {
+    console.log(error)
+  })
--- src/library/services/userServices/controller.js
+++ src/library/services/userServices/controller.js
@@ -1,21 +1,21 @@
-import Users from "../../modules/usersModule.js";
-import bcrypt from "bcrypt";
+import Users from '../../modules/usersModule.js'
+import bcrypt from 'bcrypt'
 
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await Users.find();
+    const users = await Users.find()
     if (!users) {
-      return res.status(404).json({ error: "No users found" });
+      return res.status(404).json({ error: 'No users found' })
     }
-    res.json(users);
+    res.json(users)
   } catch (error) {
-    console.log("error fetching users:", error);
-    res.status(500).send(error.message);
+    console.log('error fetching users:', error)
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const createUser = async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
   const userData = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -24,152 +24,155 @@ export const createUser = async (req, res) => {
     avatar: req.body.avatar,
     history: req.body.history,
     favorites: req.body.favorites,
-  };
+  }
   try {
-    const newUser = new Users(userData);
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    const newUser = new Users(userData)
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
   } catch (error) {
     if (error.code === 11000 && error.keyValue.email !== undefined) {
-      res.status(400).json({ error: "Email already exists" });
+      res.status(400).json({ error: 'Email already exists' })
     } else {
-      res.status(400).json({ error: `Error creating user: ${error.message}` });
+      res.status(400).json({ error: `Error creating user: ${error.message}` })
     }
   }
-};
+}
 
 export const getUserById = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
-    const user = await Users.findById(id);
+    const user = await Users.findById(id)
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    res.json(user);
+    res.json(user)
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const getUserByEmail = async (req, res) => {
-  const { email } = req.params;
+  const { email } = req.params
   try {
-    const user = await Users.findOne({ email: email });
+    const user = await Users.findOne({ email: email })
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    res.json(user);
+    res.json(user)
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const deleteUserById = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
-    const user = await Users.findByIdAndDelete(id);
+    const user = await Users.findByIdAndDelete(id)
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    res.json({ message: "User deleted" });
+    res.json({ message: 'User deleted' })
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const updateUserById = async (req, res) => {
-  const { id } = req.params;
-  const userData = req.body;
+  const { id } = req.params
+  const userData = req.body
   try {
-    const user = await Users.findByIdAndUpdate(id, userData, { new: true });
+    const user = await Users.findByIdAndUpdate(id, userData, { new: true })
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    res.json(user);
+    res.json(user)
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const loginUser = async (req, res) => {
   try {
-    const user = await Users.findOne({ email: req.body.email });
+    const user = await Users.findOne({ email: req.body.email })
     if (!user) {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
     if (await bcrypt.compare(req.body.password, user.password)) {
-    console.log("Access Token Secret:", process.env.ACCESS_TOKEN_SECRET);
-     const accessToken= jsonwebtoken.sign({ user }, process.env.ACCESS_TOKEN_SECRET); 
-     console.log(accessToken);
-      res.json({ message: "Login successful", accessToken: accessToken});
+      console.log('Access Token Secret:', process.env.ACCESS_TOKEN_SECRET)
+      const accessToken = jsonwebtoken.sign(
+        { user },
+        process.env.ACCESS_TOKEN_SECRET
+      )
+      console.log(accessToken)
+      res.json({ message: 'Login successful', accessToken: accessToken })
     } else {
-      throw new Error("Incorrect password");
+      throw new Error('Incorrect password')
     }
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const getUserByToken = async (req, res) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
   if (token === null) {
-    return res.sendStatus(401);
+    return res.sendStatus(401)
   }
   jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.sendStatus(403)
     }
-    res.json(user);
-  });
-};
+    res.json(user)
+  })
+}
 
 export const auth = (req, res, next) => {
   try {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
     if (token === null) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const decoded = jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    req.user = decoded.user;
-    next();
+    const decoded = jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    req.user = decoded.user
+    next()
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const addFavorite = async (req, res) => {
-  const { id } = req.params;
-  const { favorite } = req.body;
+  const { id } = req.params
+  const { favorite } = req.body
   try {
-    const user = await Users.findById(id);
+    const user = await Users.findById(id)
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    user.favorites.push(favorite);
-    await user.save();
-    res.json(user);
+    user.favorites.push(favorite)
+    await user.save()
+    res.json(user)
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
 
 export const changePassword = async (req, res) => {
-  const { id } = req.params;
-  const { password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { id } = req.params
+  const { password } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
   try {
     const user = await Users.findByIdAndUpdate(
       id,
       { password: hashedPassword },
       { new: true }
-    );
+    )
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' })
     }
-    res.json(user);
+    res.json(user)
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error.message)
   }
-};
+}
